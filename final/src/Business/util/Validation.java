43151ff123aa26ce0679635fc01abab45b7e3dc6
xingyu Huang <huang.xingyu2@northeastern.edu>
2021-12-12 17:42:16 -0500

    valid

--- final/src/Business/util/Validation.java
+++ final/src/Business/util/Validation.java
@@ -12,22 +12,22 @@ import javax.swing.JOptionPane;
  */
 public class Validation {
  
-     private boolean isValid(String s){
+     private static boolean isValid(String s){
         return s.matches("^[A-Za-z0-9]+$");
     }
-     private boolean isValidString(String s){
+     private static boolean isValidString(String s){
         return s.matches("^[a-zA-Z]*$");
     }
-     private boolean isValidInt(String s){
+     private static boolean isValidInt(String s){
         return s.matches("^[0-9]*$");
     }
      
-    private boolean isValidName(String s){
+    private static boolean isValidName(String s){
       return false;
     }
     
-     private boolean isValidRole(String s){
-         if(s.equals("admin")||s.equals("customer")||s.equals("deliver")) return true;
+     private  static boolean isValidRole(String s){
+         if(s.equals("Customer")||s.equals("Utility")||s.equals("Tax")||s.equals("Public")||s.equals("Medical")) return true;
          return false;
     }
 }
