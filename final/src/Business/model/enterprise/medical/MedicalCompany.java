b868d39199cdce65b8c9c526cf0cd620e9596872
liuliu <liu.liu@northeastern.edu>
2021-12-12 18:50:47 -0500

    modify

--- final/src/Business/model/enterprise/medical/MedicalCompany.java
+++ final/src/Business/model/enterprise/medical/MedicalCompany.java
@@ -5,6 +5,7 @@
 package Business.model.enterprise.medical;
 
 import Business.model.enterprise.Company;
+import java.util.Date;
 
 /**
  *
@@ -14,4 +15,78 @@ public class MedicalCompany extends Company {
      public MedicalCompany(String name) {
         super(name);
     }
+      
+       private Date payTime;
+    private Date ddl;
+
+    public Date getPayTime() {
+        return payTime;
+    }
+
+    public void setPayTime(Date payTime) {
+        this.payTime = payTime;
+    }
+
+    public Date getDdl() {
+        return ddl;
+    }
+
+    public void setDdl(Date ddl) {
+        this.ddl = ddl;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getFee() {
+        return fee;
+    }
+
+    public void setFee(Integer fee) {
+        this.fee = fee;
+    }
+
+    public String getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(String companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    private Integer id;
+    private Integer fee;
+    private String companyId;
+    private String userId;
+    private String status;
+    private String addr;
 }
