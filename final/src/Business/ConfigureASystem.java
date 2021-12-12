9009ac17380ed1ce5fc5bd2437a93d552b3fb160
liuliu <liu.liu@northeastern.edu>
2021-12-12 18:51:42 -0500

    modify

--- final/src/Business/ConfigureASystem.java
+++ final/src/Business/ConfigureASystem.java
@@ -7,15 +7,5 @@ package Business;
  */
 public class ConfigureASystem {
     
-    //public static EcoSystem configure(){
-      
-        //Create a network
-        //create an enterprise
-        //initialize some organizations
-        //have some employees 
-        //create user account
-
-        //return system;
-   // }
-    
+ 
 }
