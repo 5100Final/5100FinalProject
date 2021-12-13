6118a40edd12d0f9b8a0581d4b47a823071e82cf
ZhenLuoLF <luo.zhe@northeastern.edu>
2021-12-12 23:56:30 -0500

    Update CustomerWebView.java

--- final/src/userinterface/CustomerRole/CustomerWebView.java
+++ final/src/userinterface/CustomerRole/CustomerWebView.java
@@ -14,6 +14,10 @@ import static javafx.application.Application.launch;
 public class CustomerWebView {
       public static void main(String[] args) {
        Application.launch(CustomerViewPosition.class);
-       
+       System.out.print(args);
+       System.out.print(args);
+       System.out.print(args);
+       System.out.print(args);
+       System.out.print(args);
     }
 }
